60f0c4a59ec498b8e062752e43cba5008967e267
Emilian Peev <epeev@google.com>
2022-10-24 23:42:39 -0700

    Camera Extension stub: Add support for progress callbacks
    Bug: 255536242
    Test: Camera CTS
    Change-Id: I2c002fc3c4aab795f8cbceec3a053affcfe1ee9a

--- camera2/extensions/stub/src/main/java/androidx/camera/extensions/impl/advanced/AutoAdvancedExtenderImpl.java
+++ camera2/extensions/stub/src/main/java/androidx/camera/extensions/impl/advanced/AutoAdvancedExtenderImpl.java
@@ -88,4 +88,9 @@ public class AutoAdvancedExtenderImpl implements AdvancedExtenderImpl {
     public List<CaptureResult.Key> getAvailableCaptureResultKeys() {
         throw new RuntimeException("Stub, replace with implementation.");
     }
+
+    @Override
+    public boolean isCaptureProcessProgressAvailable() {
+        throw new RuntimeException("Stub, replace with implementation.");
+    }
 }
